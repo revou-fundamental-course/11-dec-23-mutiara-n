606e60dff8da624e373c2051810d6604829bacca
Mutiara Nafysah <Mutiara Nafysah@LAPTOP-D4K8805G>
2023-12-20 15:21:57 +0700

    validareform fix

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
 function replaceName() {
-    let name = prompt("What's your name?", "");
-    document.getElementById("name").innerHTML = name
+    let yourname = prompt("What's your name?", "");
+    document.getElementById("yourname").innerHTML = yourname;
 }
 
-document.getElementById('tombol').addEventListener("click", function() {
-    replaceName();
-})
+replaceName()
 
-let nameInput = document.getElementById('name-input');
-document.getElementById('kirim').addEventListener("click", function() {
-    const nama = nameInput.value
-})
+function setSenderUI(name, birthDate, gender, messages) {
+    document.getElementById("sender-full-name").innerHTML = name;
+    document.getElementById("sender-birth-date").innerHTML = birthDate;
+    document.getElementById("sender-gender").innerHTML = birthDate;
+    document.getElementById("sender-messages").innerHTML = messages;
+}
+
+function validateForm() {
+    const name = document.forms["message-form"]["full-name"].value;
+    const birthDate = document.forms["message-form"]["birth-date"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const messages = document.forms["message-form"]["messages"].value;
+
+    if (name == "" || birthDate == "" || gender == "" || messages == "") {
+        alert("Isi semua data");
+        return false;
+    }
+
+    setSenderUI(name, birthDate, gender, messages);
+
+    return false;
+}
