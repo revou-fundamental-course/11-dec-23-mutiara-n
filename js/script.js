6f27538063d91ead0458c252491112b28ac7e78d
Mutiara Nafysah <Mutiara Nafysah@LAPTOP-D4K8805G>
2023-12-21 14:54:26 +0700

    message us

--- js/script.js
+++ js/script.js
@@ -1,9 +1,41 @@
-function replaceName() {
-    let yourname = prompt("What's your name?", "");
-    document.getElementById("yourname").innerHTML = yourname;
+// function replaceName() {
+//     let yourname = prompt("What's your name?", "");
+//     document.getElementById("yourname").innerHTML = yourname;
+// }
+
+// replaceName()
+
+// var slideIndex = 1;
+// showDivs(slideIndex);
+
+// function plusDivs(n){
+//     showDivs ((slideIndex == n));
+// }
+
+// function showDivs(n) {
+//     var i;
+//     var imgList = document.getElementsByClassName("img-slideshow");
+//     if (n > imgList.length) slideIndex = 1;
+//     else if (n < 1) slideIndex = imgList.length;
+
+//     for (i = 0; i < imgList.length; i++) {
+//         imgList[i].style.display = "none";
+//     }
+
+//     imgList[slideIndex - 1].style.display = "block";
+// }    
+
+let currentIndex = 0
+const images = document.querySelectorAll(".slider-img")
+const totalImages = images.length
+
+function changeImage(){
+    images[currentIndex].style.display = "none"
+    currentIndex = [currentIndex + 1] % totalImages
+    images[currentIndex].style.display = "block"
 }
 
-replaceName()
+setInterval (changeImage, 2000)
 
 function setSenderUI(name, birthDate, gender, messages) {
     document.getElementById("sender-full-name").innerHTML = name;
